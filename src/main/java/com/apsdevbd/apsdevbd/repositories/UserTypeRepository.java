8fecf8073e521e8ad7d066a723bb51de1fa04b84
SamuelFST <guti.samuel2002@gmail.com>
2022-11-08 22:47:41 -0300

    removed UserType entity and repository

--- src/main/java/com/apsdevbd/apsdevbd/repositories/UserTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apsdevbd.apsdevbd.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.apsdevbd.apsdevbd.entities.UserType;
-
-public interface UserTypeRepository extends JpaRepository<UserType, Long> {
-	UserType findByName(String name);
-}
