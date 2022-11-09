8929fd6ae4ccf7a11a7bb33531ec4e09a18afb63
SamuelFST <guti.samuel2002@gmail.com>
2022-11-08 22:43:17 -0300

    getOne, update and delete endpoints for AppUser

--- src/main/java/com/apsdevbd/apsdevbd/controllers/UserController.java
+++ src/main/java/com/apsdevbd/apsdevbd/controllers/UserController.java
@@ -1,16 +1,14 @@
 package com.apsdevbd.apsdevbd.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,14 +24,22 @@ public class UserController {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-	@GetMapping("/")
+	@GetMapping("")
 	public ResponseEntity<?> listUsers() {
-		
 		if(userRepository.findAll().isEmpty()) {
-			return new ResponseEntity<>("No saved users", HttpStatus.NO_CONTENT);
-		}
-		else {
+			return new ResponseEntity<>("{ "+"\"msg\":" +"\"" +"Nenhum usuário salvo" +"\""+" }", HttpStatus.NO_CONTENT);
+		} else {
 			return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getUser(@PathVariable Long id) {
+		boolean userExists = userRepository.existsById(id);
+		if (userExists) {
+			return new ResponseEntity<AppUser>(userRepository.findById(id).get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("{ "+"\"msg\":" +"\"" +"Usuário não encontrado" +"\""+" }", HttpStatus.NOT_FOUND);			
 		}
 	}
 
@@ -46,5 +52,30 @@ public class UserController {
 		} catch (Exception err) {
 			return new ResponseEntity<>("error: " + err.getCause().getStackTrace()[0], HttpStatus.BAD_REQUEST);
 		}
+	}
+	
+	@PutMapping("")
+	public ResponseEntity<?> updateUser(@RequestBody AppUser user) {
+		try {
+			boolean userExists = userRepository.existsById(user.getId());
+			if (userExists) {
+				user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+				userRepository.saveAndFlush(user);
+				return new ResponseEntity<>("{"+"\"id\":" +user.getId() +"}", HttpStatus.OK);	
+			}
+			return new ResponseEntity<>("{ "+"\"msg\":" +"\"" +"Usuário não encontrado" +"\""+" }", HttpStatus.NOT_FOUND);				
+		} catch (Exception err) {
+			return new ResponseEntity<>("error: " + err.getCause().getStackTrace()[0], HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+		boolean userExists = userRepository.existsById(id);
+		if (userExists) {
+			userRepository.deleteById(id);
+			return new ResponseEntity<>("{"+"\"id\":" +id +"}", HttpStatus.OK);	
+		}
+		return new ResponseEntity<>("{ "+"\"msg\":" +"\"" +"Usuário não encontrado" +"\""+" }", HttpStatus.NOT_FOUND);	
 	}
 }
